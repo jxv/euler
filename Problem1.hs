c8e1244626ab32217b6a0bfee2b6ba08f34819d9
jxv <joevargas@hush.com>
2013-12-15 10:57:27 -0800

    init

---
+++ Problem1.hs
@@ -0,0 +1,21 @@
+{-
+
+If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+Find the sum of all the multiples of 3 or 5 below 1000.
+
+-}
+import Control.Applicative
+
+multipleOf :: (Integral a) => a -> a -> Bool
+multipleOf a b = snd (b `divMod` a) == 0
+
+multiples :: [Integer]
+multiples = filter (or . (<**> (multipleOf <$> [3,5])) . pure) [1..]
+
+answer :: Integer
+answer = sum (takeWhile (< 1000) multiples)
+
+main :: IO ()
+main = print answer
+
